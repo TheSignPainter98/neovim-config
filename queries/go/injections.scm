a6ee308f0cded7041f0163748bebfa4d29a1f7e4
Ed Jones <ed.jones@canonical.com>
2023-06-23 00:40:00 +0100

    Syntax highlight and format Starlark in Go

---
+++ queries/go/injections.scm
@@ -0,0 +1,10 @@
+(call_expression
+	function: (selector_expression
+		operand: (identifier) @_id (#eq? @_id "st")
+		field: (field_identifier) @_field (#eq? @_field "RunString")
+	)
+	arguments: (argument_list
+		(raw_string_literal) @python
+		; (#offset! @python 1 0 -1 0)
+	)
+)

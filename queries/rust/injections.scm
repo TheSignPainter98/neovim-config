bbd2500f9bdcb0f9cff2268bd32eecb86175bfd3
Ed Jones <ed.jones@canonical.com>
2023-07-14 19:32:24 +0100

    Syntax-highlight `sql` in `sqlx::query` calls

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -10,3 +10,17 @@
     )
   )
 )
+
+; Embedded sql
+(call_expression
+  function: (scoped_identifier
+	path: (identifier) @_pkg (#eq? @_pkg "sqlx")
+	name: (identifier) @_func (#eq? @_func "query"))
+  arguments: (arguments
+	(string_literal) @sql (#offset! @sql 1 0 -1 0))) @_call
+(call_expression
+  function: (scoped_identifier
+	path: (identifier) @_pkg (#eq? @_pkg "sqlx")
+	name: (identifier) @_func (#eq? @_func "query"))
+  arguments: (arguments
+	(raw_string_literal) @sql (#offset! @sql 3 0 -2 0))) @_call

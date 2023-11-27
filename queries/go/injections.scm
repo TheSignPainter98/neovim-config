8b9b6c0b6bc034189f17d7cbc70358ac51fc4523
Ed Jones <ed.jones@canonical.com>
2023-11-27 20:37:29 +0000

    Fix multi-line starlark highlighting

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -5,6 +5,15 @@
 		field: (field_identifier) @_field (#eq? @_field "RunString")
 	)
 	arguments: (argument_list
-		(raw_string_literal) @python (#offset! @python 1 0 -1 0)
+		(raw_string_literal) @python ; (#offset! @python 1 0 -1 0)
+	)
+)
+(call_expression
+	function: (selector_expression
+		operand: (identifier) @_id (#eq? @_id "st")
+		field: (field_identifier) @_field (#eq? @_field "SetExecutionStepModel")
+	)
+	arguments: (argument_list
+		(raw_string_literal) @python ; (#offset! @python 1 0 -1 0)
 	)
 )
